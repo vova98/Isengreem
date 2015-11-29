b7193d538b4b8792e74190cc98cf1f7ce42640aa
vova98 <rfxfyjddjdf123@mail.ru>
2015-11-29 16:52:08 +0300

    Some Repair

--- derivative/Derivative.cpp
+++ derivative/Derivative.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <cassert>
+#include <cmath>
 #include "F_work.h"
 
 
@@ -22,12 +23,12 @@
 
 #define IS(Var, vaL) !strcmp(Var, vaL)
 
-#define ITOA(Left, Right, op) itoa(atoi(Left) op atoi(Right), top->data, Radix);
+#define FTOA(Left, Right, op) top->data = ftoa(atof(Left) op atof(Right));
 
 #define NODECTOR(vertex, Data, Type, Left, Right)	\
 	vertex->type = Type;							\
 	CREATEDATA(vertex)								\
-	vertex->data = Data;							\
+	strcpy(vertex->data, Data);						\
 	vertex->left = Left;							\
 	vertex->right = Right;
 
@@ -78,17 +79,22 @@ enum Types { T_var = 1, T_op = 2, T_num = 3, T_func = 4 };
 
 const char *s = NULL;
 const int LENGTHOFWORD = 5;
+const double EPS = 0.001;
 const int Radix = 10;
 //node* NodeCtor(int type, const char op, node* left, node* right);
 node* GetG0(const char* buffer);
 int PrintTree(node* top, int* count);
 int Print(node* top, FILE* fout);
+int ÑreateTeXFile(FILE *fout);
+int IsFunc(char* val);
 node* Cleaning(node* top, int* WasCleaned);
+node* GetS();
 node* GetN();
 node* GetT();
 node* GetE();
 node* GetP();
 node* GetVar();
+node* GetFunc(char* val);
 node* Derivation(node* top);
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -99,8 +105,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		buffer = ReadFile(L"RecursParser.txt");
 	else buffer = ReadFile(argv[1]);
 	//s = (char*)buffer;
+
 	FILE* fout = NULL;
 	errno_t err = fopen_s(&fout, "Print.txt", "a");
+
+	//if (!ÑreateTeXFile(fout)) return 0;
+
 
 	node* Tree = GetG0(buffer);
 	if (Tree == NULL) { printf("Syntax ERROR\n"); return 0; }
@@ -149,6 +159,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
+
+int ÑreateTeXFile(FILE *fout)
+{
+	fprintf(fout, "\documentclass[12pt,a4paper]{scrartcl}"
+		"\\usepackage[utf8]{inputenc}"
+		"\\usepackage[english, russian]{babel}"
+		"\\usepackage{ indentfirst }"
+		"\\usepackage{ misccorr }"
+		"\\usepackage{ graphicx }"
+		"	\\usepackage{ amsmath }");
+	return OK;
+}
 /*VERIFICATOR*/
 
 //int TreeVerificator(node* top)
@@ -170,7 +192,7 @@ int PrintTree(node* top, int* count)
 		printf("----");
 	if (top->type != T_num)
 		printf("<%s>(%d) \n", TD, top->type);
-	else printf("<%d>(%d) \n", atoi(TD), top->type);
+	else printf("<%.3f>(%d) \n", atof(TD), top->type);
 
 	if (top->left  != NULL){ (*count)++; PrintTree(top->left,  count); }
 	if (top->right != NULL){ (*count)++; PrintTree(top->right, count); }
@@ -181,11 +203,21 @@ int PrintTree(node* top, int* count)
 
 int Print(node* top, FILE* fout)
 {
-	fprintf(fout, "(");
-	if (top->left) Print(top->left, fout);
-	fprintf(fout, "%s", top->data);
-	if (top->right) Print(top->right, fout);
-	fprintf(fout, ")");
+	if (top->type == T_func) 
+	{ 
+		fprintf(fout, "%s", top->data);
+		fprintf(fout, "(");
+		if (top->right) Print(top->right, fout);
+		else Print(top->left, fout);
+		fprintf(fout, ")");
+	}
+	else
+	{
+		if (top->left) { fprintf(fout, "("); Print(top->left, fout); }
+		fprintf(fout, "%s", top->data);
+		if (top->right) { Print(top->right, fout); fprintf(fout, ")"); }
+	}
+
 	return OK;
 }
 
@@ -204,32 +236,35 @@ node *GetE()
 	node* top = new node;
 	node* leaf2 = new node;
 	node* leaf = GetT();
+	if (!leaf) return NULL;
 	char op = 0;
 	while (*s == '+' || *s == '-')
 	{
 		top = new node;
 		op = *s++;
 		leaf2 = GetT();
+		if (!leaf2) return NULL;
 		CREATEDATA(top);
 		top->data[1] = '\0';
 		MAKENODE(T_op);
+
 	}
-	return top;
+	return leaf;
 }
 
 node* GetN()
 {
 	node* leaf = new node;
-	int val = 0, count = 0;
-	while (*s >= '0' && *s <= '9')
+	int count = 0;
+	char* val = (char*)calloc(SIZEOFNUMBER, sizeof(char));
+	while (*s >= '0' && *s <= '9' || *s == '.' || *s == '-')
 	{
-
-		val = val * 10 + *s - '0';
+		val[count++] =*s;
 		*s++;
-		count++;
 	}
 	(char*)leaf->data = (char*)calloc(count + 1, sizeof(char));
-	itoa(val, leaf->data, Radix);
+	strcpy(leaf->data, val);
+	//ftoa(val, leaf->data, Radix);
 	leaf->data[count] = '\0';
 	leaf->type = T_num;
 	return leaf;
@@ -239,13 +274,35 @@ node *GetT()
 {
 	node* top = new node;
 	node* leaf2 = new node;
-	node* leaf = GetP();
+	node* leaf = GetS();
+	if (!leaf) return NULL;
 	int op = 0;
 	while (*s == '/' || *s == '*')
 	{
 		top = new node;
 		op = *s++;
+		leaf2 = GetS();
+		if (!leaf2) return NULL;
+		CREATEDATA(top);
+		top->data[1] = '\0';
+		MAKENODE(T_op);
+	}
+	return leaf;
+}
+
+node *GetS()
+{
+	node* top = new node;
+	node* leaf2 = new node;
+	node* leaf = GetP();
+	if (!leaf) return NULL;
+	int op = 0;
+	if (*s == '^')
+	{
+		top = new node;
+		op = *s++;
 		leaf2 = GetP();
+		if (!leaf2) return NULL;
 		CREATEDATA(top);
 		top->data[1] = '\0';
 		MAKENODE(T_op);
@@ -261,11 +318,12 @@ node *GetP()
 		s++;
 		node* leaf = GetE();
 		if (*s == ')') { s++; return leaf; }
+		else return NULL;
 	}
 	else
 	{
 		val = GetN();
-		if (atoi(val->data) == 0) return GetVar();
+		if (atof(val->data) == NULL) return GetVar();
 		else return val;
 	}
 }
@@ -280,12 +338,34 @@ node* GetVar()
 		val[count++] = *s;
 		*s++;
 	}
-	
+	if (IsFunc(val)) 
+		return GetFunc(val);
 	(char*)leaf->data = (char*)calloc(count + 1, sizeof(char));
 	leaf->data[count] = '\0';
 	strcpy(leaf->data, val);
 	leaf->type = T_var;
 	return leaf;
+}
+
+node* GetFunc(char* val)
+{
+	node* top = new node;
+	node* leaf = GetP();
+	if (!leaf) return NULL;
+
+	CREATEDATA(top);
+	NODECTOR(top, val, T_func, leaf, NULL);
+	return top;
+}
+
+int IsFunc(char* val)
+{
+	if (IS("sin", val)) return OK;
+	else if (IS("cos", val)) return OK;
+	else if (IS("tg" , val)) return OK;
+	else if (IS("ctg", val)) return OK;
+	else if (IS("ln" , val)) return OK;
+	else return 0;
 }
 
 node* Derivation(node* top)
@@ -325,7 +405,61 @@ node* Derivation(node* top)
 
 			return newTop;
 		}
+		else if (IS(TD, "^"))
+		{
+			if (RT == T_num)
+			{
+				node* leftRightPow = new node;
+				NODECTOR(leftRightPow, RD, T_num, NULL, NULL);
+
+				node* LeftPow = new node;
+				NODECTOR(LeftPow, "*", T_op, top->left, leftRightPow);
+
+				RD = FTOA(RD, "1", -);
+
+				NODECTOR(top, "^", T_op, LeftPow, top->right);
+
+				return top;
+			}
+		}
 		break;
+	case T_func:
+		if (IS(TD, "sin"))
+		{
+			int count = 0;
+			strcpy(top->data, "cos");
+			
+			node* left = new node;
+			node* right = new node;
+			NODECTOR(right, LD, T_op, TL->left, TL->right);		
+			NODECTOR(left, TD, T_func, top->left, right);		
+			node* RightDer = Derivation(top->left);
+			
+			node* top = new node;
+			NODECTOR(top, "*", T_op, left, RightDer);
+			
+			TL->left = NULL;
+			return top;
+		}
+		if (IS(TD, "cos"))
+		{
+			strcpy(top->data, "sin");
+			node* left = new node;
+			node* right = new node;
+			NODECTOR(right, LD, T_op, TL->left, TL->right);
+
+			NODECTOR(left, TD, T_func, top->left, right);
+			node* RightDer = Derivation(top->left);
+
+			node* NewLeft = new node;
+			CREATEDATA(NewLeft);
+			NODECTOR(NewLeft, "-", T_op, NULL, left);
+
+			node* top = new node;
+			NODECTOR(top, "*", T_op, NewLeft, RightDer);
+			TL->right->left = NULL;
+			return top;
+		}
 	default: return top;
 	}
 
@@ -340,7 +474,7 @@ node* Cleaning(node* top, int* WasCleaned)
 		case T_op:
 			if (IS(TD, "+")|| IS(TD, "-"))
 			{
-				//deleting NULL in adding
+				//deleting NULL in add or sub
 				if (IS(TD, "+") && IS(LD, "0") && !IS(RD, "0"))
 				{
 					top = top->right;
@@ -351,23 +485,23 @@ node* Cleaning(node* top, int* WasCleaned)
 					top = top->left;
 					*WasCleaned = 1;
 				}
-				else if (IS(TD, "-")&& IS(RD, "0"))
+				else if (IS(TD, "-") && IS(RD, "0"))
 				{
 					top = top->left;
 					*WasCleaned = 1;
 				}
 				// constant folding
-				else if (top->left->type == T_num && top->right->type == T_num)
+				else if (TL && TR && LT == T_num && RT == T_num)
 				{
 					top->type = T_num;
 					if (IS(TD, "+"))
 					{
 						top->data = new char[LENGTHOFWORD];
-						ITOA(LD, RD, +);
+						FTOA(LD, RD, +);
 					}
 					else {
 						top->data = new char[LENGTHOFWORD];
-						ITOA(LD, RD, -); 
+						FTOA(LD, RD, -); 
 					}
 					top->left = NULL;
 					top->right = NULL;
@@ -398,7 +532,8 @@ node* Cleaning(node* top, int* WasCleaned)
 				else if (LT == T_num && RT == T_num)
 				{
 					top->data = new char[LENGTHOFWORD];
-					NODECTOR(top, ITOA(LD, RD, *), T_num, NULL, NULL);
+					top->data = FTOA(LD, RD, *);
+					NODECTOR(top, top->data, T_num, NULL, NULL);
 					*WasCleaned = 1;
 				}
 			}
@@ -412,7 +547,8 @@ node* Cleaning(node* top, int* WasCleaned)
 				else if (LT == T_num && RT == T_num)
 				{
 					top->data = new char[LENGTHOFWORD];
-					NODECTOR(top, ITOA(LD, RD, / ), T_num, NULL, NULL);
+					top->data = FTOA(LD, RD, / );
+					NODECTOR(top, top->data, T_num, NULL, NULL);
 					*WasCleaned = 1;
 				}
 				else if (IS(RD, "1"))
@@ -420,6 +556,27 @@ node* Cleaning(node* top, int* WasCleaned)
 					top = top->left;
 					*WasCleaned = 1;
 				}
+			}
+			else if (IS(TD, "^"))
+			{
+				if (LT == T_num && RT == T_num)
+				{
+					float tmp = pow(atof(LD), atof(RD));
+					char* newData = ftoa(tmp);
+					NODECTOR(top, newData, T_num, NULL, NULL);
+					*WasCleaned = 1;
+				}
+				else if (LT != T_num && abs(atof(RD) - 1)<EPS)
+				{
+					top = top->left;
+					*WasCleaned = 1;
+				}
+				else if (RT != T_num && abs(atof(LD) - 1)<EPS)
+				{
+					NODECTOR(top, "1", T_num, NULL, NULL);
+					*WasCleaned = 1;
+				}
+
 			}
 			top->left = Cleaning(top->left, WasCleaned);
 			top->right = Cleaning(top->right, WasCleaned);
